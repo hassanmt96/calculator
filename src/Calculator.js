7ccc149db6476327f725d899a6f01a2ca16f6bb3
Adam <warwickjunglex@gmail.com>
2020-11-11 16:07:36 -0500

    Phase 4 complete

--- src/Calculator.js
+++ src/Calculator.js
@@ -10,12 +10,25 @@ class Calculator extends React.Component {
         } 
     }
 
+    handleFirstNum = e => {
+        const parseNum = parseInt(e.target.value, 10);
+        const num1 = isNaN(parseNum) ? '' : parseNum;
+        this.setState({ num1 });
+    };
+    handleSecNum = e => {
+        const parseNum = parseInt(e.target.value, 10);
+        const num2 = isNaN(parseNum) ? '' : parseNum;
+        this.setState({ num2 });
+    };
+
     render(){
         return(
             <div>
-                <h1>Time for math!</h1>
+                <h1>Result: {this.state.result}</h1>
+                <input onChange={this.handleFirstNum} value={this.state.num1} placeholder='First number'/>
+                <input onChange={this.handleSecNum} value={this.state.num2} placeholder='Second number'/>
             </div>
-        )
+        );
     }
 }
 
