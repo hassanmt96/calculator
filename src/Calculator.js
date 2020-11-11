e4b343ed860e7a21819b9a0b74b535f6c8aad7e3
Adam <warwickjunglex@gmail.com>
2020-11-11 16:19:58 -0500

    Complete

--- src/Calculator.js
+++ src/Calculator.js
@@ -15,18 +15,51 @@ class Calculator extends React.Component {
         const num1 = isNaN(parseNum) ? '' : parseNum;
         this.setState({ num1 });
     };
+
     handleSecNum = e => {
         const parseNum = parseInt(e.target.value, 10);
         const num2 = isNaN(parseNum) ? '' : parseNum;
         this.setState({ num2 });
     };
 
+    add = () => {
+        const result = this.state.num1 + this.state.num2;
+        this.setState({ result });
+    };
+    
+    minus = () => {
+        const result = this.state.num1 - this.state.num2;
+        this.setState({ result });
+    };
+    
+    multiply = () => {
+        const result = this.state.num1 * this.state.num2;
+        this.setState({ result });
+    };
+    
+    divide = () => {
+        const result = this.state.num1 / this.state.num2;
+        this.setState({ result });
+    };
+    
+    clear = () => {
+        this.setState({ num1: '', num2: '', result: 0 });
+    };
+
     render(){
+        const { num1, num2, result } = this.state;
+
         return(
             <div>
-                <h1>Result: {this.state.result}</h1>
-                <input onChange={this.handleFirstNum} value={this.state.num1} placeholder='First number'/>
-                <input onChange={this.handleSecNum} value={this.state.num2} placeholder='Second number'/>
+                <h1>Result: {result}</h1>
+                <input onChange={this.handleFirstNum} value={num1} placeholder='First number'/>
+                <input onChange={this.handleSecNum} value={num2} placeholder='Second number'/>
+                <br/>
+                <button onClick={this.add}>+</button> 
+                <button onClick={this.minus}>-</button> 
+                <button onClick={this.multiply}>*</button> 
+                <button onClick={this.divide}>/</button>
+                <button onClick={this.clear}>clear</button>
             </div>
         );
     }
